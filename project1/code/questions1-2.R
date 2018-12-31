e2dce4f9f7f431238f890bd8765b6ae8b122e957
dominik <dominik.samorek@tooploox.com>
2018-12-31 17:46:56 +0100

    new models for question 4, all checked for convergence and fit, best model found

--- project1/code/questions1-2.R
+++ project1/code/questions1-2.R
@@ -8,38 +8,42 @@ for (idi in NIC$id) lines(NIC$age[NIC$id==idi], NIC$bw[NIC$id==idi]);
 # model for questions 1 and 2
 cat("
     model {
-      for (i in 1:N) {
-        body_weight[i] ~ dnorm(body.mu[i], body.tau)
-        body.mu[i] <- beta.const + beta.breed*breed[i] + beta.age*age[i] + b[id[i],1] + b[id[i],2]*age[i]
-      }
-
-      for (i in 1:M) {
-        b[i, 1:2] ~ dmnorm(b_mean[1:2], b.tau[1:2,1:2])
-      }
-
-      # error SD
-      body.tau ~ dgamma(0.001,0.001)
-      body.sigma <- 1 / sqrt(body.tau)
-
-      # fixed effects
-      beta.const ~ dnorm(0, 1.0E-6)
-      beta.breed ~ dnorm(0, 1.0E-6)
-      beta.age ~ dnorm(0, 1.0E-6)
-
-      # random effects
-      b.tau ~ dwish(b_R[1:2,1:2], 2)
-      b.sigma2[1:2,1:2]  <- inverse(b.tau[1:2,1:2])
-      for (i in 1:2) {
-        b.sigma[i] <- sqrt(b.sigma2[i,i])
-      }
-      b.corr <- b.sigma2[1,2]/(b.sigma[1]*b.sigma[2])
-
-      error <- mean(body_weight - body.mu)
-      b.intercept.means <- mean(b[,1])
-      b.slope.means <- mean(b[,2])
-      
-  
-    }", file="./project1/code/jags-programs/NIC-model1.jag")
+    for (i in 1:N) {
+    body_weight[i] ~ dnorm(body.mu[i], body.tau)
+    body.mu[i] <- beta.const + beta.breed*breed[i] + beta.age*age[i] + b[id[i],1] + b[id[i],2]*age[i]
+    }
+    
+    for (i in 1:M) {
+    b[i, 1:2] ~ dmnorm(b_mean[1:2], b.tau[1:2,1:2])
+    }
+    
+    # error SD
+    body.tau ~ dgamma(0.00001,0.00001)
+    body.sigma <- 1 / sqrt(body.tau)
+    
+    # fixed effects
+    beta.const ~ dnorm(0, 1.0E-6)
+    beta.breed ~ dnorm(0, 1.0E-6)
+    beta.age ~ dnorm(0, 1.0E-6)
+    
+    # random effects
+    b.sigma[1] ~ dunif(0,2)
+    b.sigma[2] ~ dunif(0,2)
+    b.corr ~ dunif(0,1)
+    
+    for (i in 1:2) {
+    b.sigma2[i,i] <- pow(b.sigma[i], 2)
+    }
+    b.sigma2[1,2] <- b.sigma[1] * b.sigma[2] * b.corr
+    b.sigma2[2,1] <- b.sigma[1] * b.sigma[2] * b.corr
+    
+    b.tau <- inverse(b.sigma2[1:2,1:2])
+    
+    error <- mean(body_weight - body.mu)
+    b.intercept.means <- mean(b[,1])
+    b.slope.means <- mean(b[,2])
+    
+    }", file="./project1/code/jags-programs/NIC-norm-uni-model.jag")
 
 NIC_data <- list(
   body_weight=normalize(NIC$bw),
@@ -76,12 +80,12 @@ NIC_inits <- list(
 # model fitting
 NIC_model_question1 <- jags(
   NIC_data,
-  parameters.to.save=c("beta.const", "beta.breed", "beta.age", "body.sigma", "b.sigma", "b.corr"),
-  inits=NIC_inits,
+  parameters.to.save=c("beta.breed", "beta.age", "body.sigma", "b.sigma", "b.corr"),
+  #  inits=NIC_inits,
   n.iter=50000,
   n.chains=3,
   n.burnin=25000,
-  model.file="./project1/code/jags-programs/NIC-model1.jag",
+  model.file="./project1/code/jags-programs/NIC-norm-uni-model.jag",
   n.thin=1,
   DIC=T
 )
@@ -91,8 +95,8 @@ NIC_model_MCMC <- as.mcmc(NIC_model_question1)
 NIC_model_gg <- ggs(NIC_model_MCMC)
 print(NIC_model_question1)
 summary(NIC_model_MCMC)
-ggmcmc(NIC_model_gg, "./project1/question1_plots.pdf")
-superdiag(NIC_model_MCMC, burnin = 10000)
+ggmcmc(NIC_model_gg, "./project1/question1_plots-norm-uni-distribution.pdf")
+superdiag(NIC_model_MCMC, burnin = 1000)
 
 
 # question 2
@@ -105,7 +109,7 @@ NIC_model_question2 <- jags(
   n.iter=50000,
   n.chains=3,
   n.burnin=25000,
-  model.file="./project1/code/jags-programs/NIC-model1.jag",
+  model.file="./project1/code/jags-programs/NIC-norm-uni-model.jag",
   n.thin=1,
   DIC=T
 )
